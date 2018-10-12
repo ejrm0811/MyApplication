952c2499e04d609d08ddb3341458f3a6e3af3b43
Shin donghyeok <35420463+ejrm0811@users.noreply.github.com>
2018-10-12 21:47:49 +0900

    a

---
+++ app/src/main/java/com/developer/mrshinsa/myapplication/Order.java
@@ -0,0 +1,17 @@
+package com.developer.mrshinsa.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+
+
+}
+
+
